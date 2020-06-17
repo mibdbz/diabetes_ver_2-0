597df74b42f196bc062ede24ca29064243196d71
mibdbz <mibdbz@o2.pl>
2020-06-17 10:50:58 +0200

    ProductDAO and it's implementation created

---
+++ diabetes/src/pl/mibdbz/diabetes/dao/ProductDAO.java
@@ -0,0 +1,10 @@
+package pl.mibdbz.diabetes.dao;
+
+import java.util.List;
+
+import pl.mibdbz.diabetes.entity.Product;
+
+public interface ProductDAO {
+
+	public List<Product> getProducts();
+} 
---
+++ diabetes/src/pl/mibdbz/diabetes/dao/ProductDAOImpl.java
@@ -0,0 +1,34 @@
+package pl.mibdbz.diabetes.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import pl.mibdbz.diabetes.entity.Product;
+
+
+@Repository
+public class ProductDAOImpl implements ProductDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Product> getProducts() {
+
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Query<Product> theQuery = currentSession.createQuery("From Product", Product.class);
+		
+		List<Product> products = theQuery.getResultList();
+		
+		return products;
+	}
+
+}
