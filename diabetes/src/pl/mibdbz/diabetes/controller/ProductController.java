b74d3dd3b713a6682e0c30b2582661023efcb6fd
mibdbz <mibdbz@o2.pl>
2020-06-18 11:54:27 +0200

    Refactoring productList method in controller

--- diabetes/src/pl/mibdbz/diabetes/controller/ProductController.java
+++ diabetes/src/pl/mibdbz/diabetes/controller/ProductController.java
@@ -1,15 +1,29 @@
 package pl.mibdbz.diabetes.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pl.mibdbz.diabetes.dao.ProductDAO;
+import pl.mibdbz.diabetes.entity.Product;
+
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+	
+	@Autowired
+	private ProductDAO productDAO;
+	
 
 	@RequestMapping("/list")
 	public String productsList(Model theModel) {
+		
+		List<Product> theProducts = productDAO.getProducts();
+		
+		theModel.addAttribute("products", theProducts);
 		
 		return "products-list";
 	}
